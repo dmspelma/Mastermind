e3019165ede9436c7e769419d7dd0f7d0d10d09e
Drew Spelman <drewspelman@gmail.com>
2022-03-27 20:36:10 -0700

    Editing existing tests, clearing up methods by adding comments and making formats cleaner

--- lib/mastermind_solver/solver.rb
+++ lib/mastermind_solver/solver.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require_relative '../mastermind/mastermind_owner'
-require_relative '../../helper/color_options_choice' # Includes OPTIONS hash which refers to colors
-require_relative '../../helper/string_color_helper' # Adds colors to output
-require_relative '../../helper/mastermind_solver/solver_first_guess_helper' # helps decide first guess after base guess
+require_relative '../../helper/color_options_choice' # Include OPTIONS hash which refers to colors.
+require_relative '../../helper/string_color_helper' # Adds color to output.
+require_relative '../../helper/mastermind_solver/solver_first_guess_helper' # picks second guess.
 require_relative '../../helper/game_settings_helper'
 require 'benchmark'
 
@@ -33,23 +33,19 @@ module MastermindSolver
         puts 'You have already solved! You need to perform `.restart` before you can try again.'
         return false
       end
-      # starting guess is based off of Wikipedia's Five-guess algorithm
+
       guess = @owner.version == :regular ? %w[R R G B] : %w[R R G G B]
       a = 1
       loop do
-        # print "taking guess: ".yellow
-        # print "#{guess}".white
         x = @owner.compare_guess(guess)
-        # puts ", result is: #{x}"
         @turns_to_solve += 1
         break if x == true
 
         # remove from S any code that would not give the same response if it were the code.
         prune_set(@solutions_set, guess, x)
-        # Apply minimax theory
+
+        # Call @choice if a == 1 (our second guess). Otherwise, call minimax
         guess = a == 1 ? @choice[x] : minimax
-        # update the guess based on applying minimax.
-        # Minimax will call to find best guess to return.
         var[[x, guess]] += 1 if a == 1
         a += 1
       end
@@ -69,6 +65,7 @@ module MastermindSolver
       @state = :unsolved
     end
 
+    # For obtaining results of benchmarking solving + restart functions.
     def benchmark(number_of_tests, var = Hash.new(0))
       if number_of_tests.class != Integer || number_of_tests <= 0
         puts 'Method only accepts positive numbers'
@@ -85,7 +82,7 @@ module MastermindSolver
       calculate_and_return(time, number_of_tests)
     end
 
-    # private
+    private
 
     def calculate_and_return(my_info, number_of_tests)
       answer = [0, 0, 0] # [total_time, turns, max_turn]
@@ -156,11 +153,11 @@ module MastermindSolver
     end
 
     # Finds the Maximum number of possible correct `guesses`
-    # Then, it chooses the `guess` that provies the smallest number of possible changes
+    # Then, it chooses the `guess` that is best to take next.
     def minimax
       next_guesses = [] # This is what will be returned. An array of guesses to take.
       score = {} # This will hold a map of guesses => max_score
-      map = Hash.new(0) # This will hold a map of score_sets => scores.
+      map = Hash.new(0) # This will hold a map of score_sets (eg. [1,0]) => number seen.
       min = max = 0
       @solutions_set.each do |full_answer|
         @solutions_set.each do |possible_answer|
@@ -176,25 +173,16 @@ module MastermindSolver
       end
       min = score.values.max
       score.each do |key, value|
-        # puts "Score |key, value| = |#{key}, #{value}|"
         next_guesses << key if value == min
       end
-      # puts "score is: #{score}"
-      # puts "next_guess is: #{next_guesses}"
       get_next_guess(next_guesses) # Return best guess from list. Prioritizes lowest
     end
 
     # returns the first guess that exists.
     def get_next_guess(guesses)
-      # first checks @solutions_set for guess.
       guesses.each do |g|
         return g if @solutions_set.include?(g)
       end
-
-      # If not found above, then checks @answer_set.
-      #guesses.each do |g|
-      #  return g if @answer_set.include?(g)
-      #end
     end
   end
 end
